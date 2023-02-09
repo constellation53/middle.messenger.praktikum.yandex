86860ddaa59cab05eb9bbd51b7a218792d9120ea
constellation53 <almahanov567@gmail.com>
2023-02-10 02:37:20 +0600

    feat(auth): add first test page

---
+++ src/modules/test.js
@@ -0,0 +1,7 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import * as Handlebars from 'handlebars';
+import * as path from 'path';
+
+const partial_path = path.join(HBS, partial);
+const { name } = path.parse(path.basename(partial_path));
+Handlebars.registerPartial(name, readHbs(partial_path));
