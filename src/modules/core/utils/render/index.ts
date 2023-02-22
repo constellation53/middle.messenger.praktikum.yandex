a05c304adf8ae3c02fb8bcd005f2c0e13b6b88f8
constellation53 <almahanov567@gmail.com>
2023-02-23 00:51:37 +0600

    feat(core): add new util

---
+++ src/modules/core/utils/render/index.ts
@@ -0,0 +1,12 @@
+import Block from '../block';
+
+export const render = (query: string, block: Block): Element | null => {
+  const root = document.querySelector(query);
+
+
+  root?.appendChild(block.getContent()!);
+
+  block.dispatchComponentDidMount();
+
+  return root;
+};
