466a7ed92e3d3356b6962cb5bb473fe21a874fc8
constellation53 <almahanov567@gmail.com>
2023-03-03 21:07:17 +0600

    refactor(core): change structure

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,26 @@
 import { render } from './modules/core/utils/render';
-import { MainPage } from "./pages/main";
+import Block from './modules/core/utils/block';
+import { NavComponent } from './modules/core/components/nav';
+import { routes } from './modules/core/config/routes';
+import template from './index.hbs';
+import styles from './styles/index.module.scss';
+
+export class MainPage extends Block {
+  constructor() {
+    super();
+  }
+
+  init(): void {
+    this.children.navigation = new NavComponent({ routes });
+  }
+
+  render(): DocumentFragment {
+    return this.compile(template, {
+      ...this.props,
+      styles,
+    });
+  }
+}
 
 const mainPage = new MainPage();
 
