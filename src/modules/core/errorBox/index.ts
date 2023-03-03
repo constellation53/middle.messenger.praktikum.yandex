3d49f0384188615ee549dd831ae06484b2428f9f
constellation53 <almahanov567@gmail.com>
2023-03-03 22:43:43 +0600

    refactor(core): use precompile for errorBox

---
+++ src/modules/core/errorBox/index.ts
@@ -0,0 +1,25 @@
+import Block from '../../core/utils/block';
+import { ErrorBoxType } from './types';
+import { Button } from '../button';
+import template from './index.hbs';
+import styles from './styles/index.module.scss';
+
+type PropsType = ErrorBoxType;
+export class ErrorBox extends Block<PropsType> {
+  constructor(props: PropsType) {
+    super(props);
+  }
+
+  init(): void {
+    this.children.button = new Button({
+      text: this.props.buttonProps.text,
+      href: this.props.buttonProps.href,
+      secondary: true,
+      class: 'size-12',
+    });
+  }
+
+  render(): DocumentFragment {
+    return this.compile(template, { ...this.props, styles });
+  }
+}
