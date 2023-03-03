cb2edfaeb313caaeea7dd6b498d53ef593633c32
constellation53 <almahanov567@gmail.com>
2023-03-03 20:01:42 +0600

    refactor(core): add main page

--- src/pages/main/index.ts
+++ src/pages/main/index.ts
@@ -1,12 +1,19 @@
 import Block from '../../modules/core/utils/block';
 import template from './index.hbs';
+import { NavComponent } from '../../modules/core/components/nav';
+import { routes } from '../../modules/core/config/routes';
+import styles from './styles/index.module.scss';
 
 export class MainPage extends Block {
   constructor() {
-    super('section');
+    super({});
+  }
+
+  init(): void {
+    this.children.navigation = new NavComponent({ routes });
   }
 
   render(): DocumentFragment {
-    return this.compile(template);
+    return this.compile(template, { ...this.props, styles });
   }
 }
