ccd2775bfd0aa361835462b1092a918baefcbcf5
constellation53 <almahanov567@gmail.com>
2023-03-03 22:54:17 +0600

    refactor(core): use precompile for notFound page

---
+++ src/pages/notFound/index.ts
@@ -0,0 +1,30 @@
+import Block from '../../modules/core/utils/block';
+import { ErrorBox } from '../../modules/core/elements/errorBox';
+import template from './index.hbs';
+import { render } from '../../modules/core/utils/render';
+import styles from './styles/index.module.scss'
+
+export class NotFoundPage extends Block {
+  constructor() {
+    super();
+  }
+
+  init(): void {
+    this.children.errorBox = new ErrorBox({
+      code: 404,
+      text: 'Не туда попали',
+      buttonProps: {
+        text: 'Назад к чатам',
+        href: 'test',
+      },
+    });
+  }
+
+  render(): DocumentFragment {
+    return this.compile(template, { styles });
+  }
+}
+
+const notFoundPage = new NotFoundPage();
+
+render('#root', notFoundPage);
