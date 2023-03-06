b25fe680e54d1ed19042a8cd377fbe8ae3bad48a
constellation53 <almahanov567@gmail.com>
2023-03-06 20:33:20 +0600

    refactor(auth): connect form

--- src/pages/signUp/index.ts
+++ src/pages/signUp/index.ts
@@ -4,6 +4,9 @@ import Block from '../../modules/core/utils/block';
 // Templates
 import template from './index.hbs';
 
+// Components
+import { RegistrationFormComponent } from '../../modules/auth/registrationForm';
+
 // Other
 import { render } from '../../modules/core/utils/render';
 import styles from './styles/index.module.scss';
@@ -11,6 +14,10 @@ import styles from './styles/index.module.scss';
 export class SignUpPage extends Block {
   constructor() {
     super();
+  }
+
+  init(): void {
+    this.children.registrationForm = new RegistrationFormComponent()
   }
 
   render(): DocumentFragment {
