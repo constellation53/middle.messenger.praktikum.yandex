e5199f7edef2794b4d28938e1a74bf034ca27f94
constellation53 <almahanov567@gmail.com>
2023-03-06 19:51:28 +0600

    refactor(core): remove excess helper

--- src/modules/core/helpers/is/index.ts
+++
@@ -1,5 +0,0 @@
-import Handlebars, { HelperOptions } from "handlebars";
-
-Handlebars.registerHelper('is', (arg1: unknown, arg2: unknown, options: HelperOptions)  => {
-  return (arg1 === arg2) ? options.fn(this) : options.inverse(this);
-});
