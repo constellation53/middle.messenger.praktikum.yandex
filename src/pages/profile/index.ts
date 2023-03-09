52f68f1d62945720e0b6b4848ea341d2003732e6
constellation53 <almahanov567@gmail.com>
2023-03-09 23:02:31 +0600

    refactor(profile): connect avatar

--- src/pages/profile/index.ts
+++ src/pages/profile/index.ts
@@ -7,10 +7,19 @@ import template from './index.hbs';
 // Other
 import { render } from '../../modules/core/utils/render';
 import * as styles from './styles/index.module.scss';
+import { Avatar } from '../../modules/core/elements/avatar';
 
 export class ProfilePage extends Block {
   constructor() {
     super();
+  }
+
+  init(): void {
+    this.children.avatar = new Avatar({
+      medium: true,
+      editable: true,
+      alt: 'Загрузить фото',
+    });
   }
 
   render(): HTMLElement {
