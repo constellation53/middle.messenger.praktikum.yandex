292f7cb2bd21eaa8094f77f8133bfb625a3dae8b
constellation53 <almahanov567@gmail.com>
2023-03-13 13:32:12 +0600

    refactor(profile): add profileEdit page

---
+++ src/pages/profileEdit/index.ts
@@ -0,0 +1,37 @@
+// Infrastructure
+import Block from '../../modules/core/utils/block';
+
+// Templates
+import template from './index.hbs';
+
+// Components
+import { ProfileInfoFormComponent } from '../../modules/profile/components/profileInfoForm';
+
+// Other
+import { render } from '../../modules/core/utils/render';
+import * as styles from './styles/index.module.scss';
+import { Avatar } from '../../modules/core/elements/avatar';
+
+export class ProfilePage extends Block {
+  constructor() {
+    super();
+  }
+
+  init(): void {
+    this.children.avatar = new Avatar({
+      medium: true,
+      editable: true,
+      alt: 'Загрузить фото',
+    });
+
+    this.children.profileInfoForm = new ProfileInfoFormComponent();
+  }
+
+  render(): HTMLElement {
+    return this.compile(template, { styles });
+  }
+}
+
+const profilePage = new ProfilePage();
+
+render('#root', profilePage);
