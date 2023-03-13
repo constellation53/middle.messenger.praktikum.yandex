a8922f216577fed50cdea37e3470df2787c86c1a
constellation53 <almahanov567@gmail.com>
2023-03-13 12:12:10 +0600

    refactor(profile): connect buttons

--- src/pages/profile/index.ts
+++ src/pages/profile/index.ts
@@ -9,6 +9,8 @@ import { render } from '../../modules/core/utils/render';
 import * as styles from './styles/index.module.scss';
 import { Avatar } from '../../modules/core/elements/avatar';
 import { ProfileInfoFormComponent } from '../../modules/profile/components/profileInfoForm';
+import { Button } from '../../modules/core/elements/button';
+import { Divider } from '../../modules/core/elements/divider';
 
 export class ProfilePage extends Block {
   constructor() {
@@ -22,7 +24,33 @@ export class ProfilePage extends Block {
       alt: 'Загрузить фото',
     });
 
-    this.children.profileInfoForm = new ProfileInfoFormComponent({ disabled: true});
+    this.children.profileInfoForm = new ProfileInfoFormComponent({disabled: true,});
+
+    // TODO: ADD LINKS
+    this.children.changeProfileInfoButton = new Button({
+      text: 'Изменить данные',
+      secondary: true,
+      alignLeft: true,
+      fluid: true,
+    });
+
+    this.children.changeProfileInfoDivider = new Divider();
+
+    this.children.changeProfilePasswordButton = new Button({
+      text: 'Изменить пароль',
+      secondary: true,
+      alignLeft: true,
+      fluid: true,
+    });
+
+    this.children.changeProfileePasswordDivider = new Divider()
+
+    this.children.logoutButton = new Button({
+      text: 'Выйти',
+      tertiary: true,
+      alignLeft: true,
+      fluid: true,
+    });
   }
 
   render(): HTMLElement {
