999f9c195cfee3dc1dc990c30c940a48c0983e39
constellation53 <almahanov567@gmail.com>
2023-03-15 15:46:20 +0600

    refactor(button): chanage typingc

--- src/modules/core/elements/button/types/index.ts
+++ src/modules/core/elements/button/types/index.ts
@@ -3,6 +3,14 @@ type VariantNameType = 'primary' | 'secondary' | 'tertiary';
 
 type VariantType = Partial<Record<VariantNameType, boolean>>;
 
+type RequireOnlyOne<T, Keys extends keyof T = keyof T> = Pick<
+  T,
+  Exclude<keyof T, Keys>
+> &
+  {
+    [K in Keys]-?: Pick<T, K> & Partial<Record<Exclude<Keys, K>, never>>;
+  }[Keys];
+
 type AlignType = Partial<Record<AlignNameType, boolean>>;
 
 type EventsType = {
@@ -14,7 +22,7 @@ type CoreType = {
   class?: string;
   fluid?: boolean;
   underline?: boolean;
-} & VariantType &
+} & RequireOnlyOne<VariantType> &
   AlignType;
 
 type LinkButtonType = CoreType & {
