a94309472497f2c5dee018570e737760375647c5
constellation53 <almahanov567@gmail.com>
2023-03-15 11:55:57 +0600

    refactor(avatar): change typing

--- src/modules/core/elements/avatar/index.ts
+++ src/modules/core/elements/avatar/index.ts
@@ -7,11 +7,12 @@ import template from './index.hbs';
 // Other
 import { AvatarType } from './types';
 import * as styles from './styles/index.module.scss';
+import { CoreBlockType } from '../../utils/block/types';
 
 type PropsType = AvatarType;
 
 export class Avatar extends Block<PropsType> {
-  constructor(props: PropsType) {
+  constructor(props: CoreBlockType<PropsType>) {
     super(props);
   }
 
