340a7313772a7234560e210fd1ece219f06c1fbb
constellation53 <almahanov567@gmail.com>
2023-03-17 14:41:14 +0600

    feat(chat): add base list

--- src/modules/chat/components/chatList/index.ts
+++ src/modules/chat/components/chatList/index.ts
@@ -7,18 +7,198 @@ import template from './index.hbs';
 // Components
 import { LinkComponent } from './link';
 import { SearchFormComponent } from './searchForm';
+import { ChatItemComponent } from './chatItem';
 
 // Other
 import * as styles from './styles/index.module.scss';
+import { ChatItemType } from './types';
+
+const chats: ChatItemType[] = [
+  {
+    id: 123,
+    title: 'Андрей',
+    avatar: null,
+    unreadCount: 15,
+    lastMessage: {
+      user: {
+        firstName: 'Андрей',
+        secondName: 'Pupkin',
+        avatar: null,
+        email: 'my@email.com',
+        login: 'userLogin',
+        phone: '8(911)-222-33-22',
+      },
+      time: '10:49',
+      content: 'Изображение',
+    },
+  },
+  {
+    id: 1234,
+    title: 'Киноклуб',
+    avatar: null,
+    unreadCount: null,
+    lastMessage: {
+      user: {
+        firstName: 'Nurzhan',
+        secondName: 'Pupkin',
+        avatar: null,
+        email: 'my@email.com',
+        login: 'userLogin',
+        phone: '8(911)-222-33-22',
+      },
+      time: '12:00',
+      content: 'Cтикер',
+    },
+  },
+  {
+    id: 123,
+    title: 'Илья',
+    avatar: null,
+    unreadCount: 4,
+    lastMessage: {
+      user: {
+        firstName: 'Илья',
+        secondName: 'Pupkin',
+        avatar: null,
+        email: 'my@email.com',
+        login: 'userLogin',
+        phone: '8(911)-222-33-22',
+      },
+      time: '15:12',
+      content: 'Друзья, у меня для вас особенный выпуск новостей!'
+        + 'Друзья, у меня для вас особенный выпуск новостей!'
+        + 'Друзья, у меня для вас особенный выпуск новостей!',
+    },
+  },
+  {
+    id: 1234,
+    title: 'Вадим',
+    avatar: null,
+    unreadCount: null,
+    lastMessage: {
+      user: {
+        firstName: 'Nurzhan',
+        secondName: 'Pupkin',
+        avatar: null,
+        email: 'my@email.com',
+        login: 'userLogin',
+        phone: '8(911)-222-33-22',
+      },
+      time: 'Пт',
+      content: 'Круто!',
+    },
+  },
+  {
+    id: 123,
+    title: 'тет-а-теты',
+    avatar: null,
+    unreadCount: null,
+    lastMessage: {
+      user: {
+        firstName: 'Илья',
+        secondName: 'Pupkin',
+        avatar: null,
+        email: 'my@email.com',
+        login: 'userLogin',
+        phone: '8(911)-222-33-22',
+      },
+      time: 'Ср',
+      content: 'И Human Interface Guidelines и Material Design '
+        + 'рекомендуют И Human Interface Guidelines и Material Design '
+        + 'рекомендуют И Human Interface Guidelines и Material Design '
+        + 'рекомендуют',
+    },
+  },
+  {
+    id: 123,
+    title: '1, 2, 3',
+    avatar: null,
+    unreadCount: null,
+    lastMessage: {
+      user: {
+        firstName: 'Илья',
+        secondName: 'Pupkin',
+        avatar: null,
+        email: 'my@email.com',
+        login: 'userLogin',
+        phone: '8(911)-222-33-22',
+      },
+      time: 'Пн',
+      content: 'Миллионы россиян ежедневно проводят десятки '
+        + 'часов свое Миллионы россиян ежедневно проводят'
+        + 'десятки часов свое Миллионы россиян ежедневно '
+        + 'проводят десятки часов свое',
+    },
+  },
+  {
+    id: 123,
+    title: 'Design Destroyer',
+    avatar: null,
+    unreadCount: null,
+    lastMessage: {
+      user: {
+        firstName: 'Илья',
+        secondName: 'Pupkin',
+        avatar: null,
+        email: 'my@email.com',
+        login: 'userLogin',
+        phone: '8(911)-222-33-22',
+      },
+      time: 'Пн',
+      content: 'В 2008 году художник Jon Rafman  начал собирать '
+        + 'В 2008 году художник Jon Rafman  начал собирать '
+        + 'В 2008 году художник Jon Rafman  начал собирать',
+    },
+  },
+  {
+    id: 123,
+    title: 'Day.',
+    avatar: null,
+    unreadCount: 999,
+    lastMessage: {
+      user: {
+        firstName: 'Илья',
+        secondName: 'Pupkin',
+        avatar: null,
+        email: 'my@email.com',
+        login: 'userLogin',
+        phone: '8(911)-222-33-22',
+      },
+      time: '1 Мая 2020',
+      content: 'Так увлёкся работой по курсу, что совсем забыл его анонсир '
+        + 'Так увлёкся работой по курсу, что совсем забыл его анонси '
+        + 'Так увлёкся работой по курсу, что совсем забыл его анонсир ',
+    },
+  },
+];
 
 export class ChatListComponent extends Block {
   constructor() {
     super();
   }
 
+  prepareList(list: ChatItemType[]): Block<ChatItemType>[] {
+    return list.map(
+      ({
+        title,
+        id,
+        avatar,
+        unreadCount,
+        lastMessage,
+      }) => new ChatItemComponent({
+        title,
+        id,
+        avatar,
+        unreadCount,
+        lastMessage,
+      }),
+    );
+  }
+
   init(): void {
     this.children.link = new LinkComponent();
     this.children.search = new SearchFormComponent();
+    this.children.list = this.prepareList(chats);
   }
 
   render(): HTMLElement {
---
+++ src/modules/chat/components/chatList/types/index.ts
@@ -0,0 +1,20 @@
+type UserType = {
+  firstName: string;
+  secondName: string;
+  avatar: string | null;
+  email: string;
+  login: string;
+  phone: string;
+};
+
+export type ChatItemType = {
+  id: number;
+  title: string;
+  avatar: string | null;
+  unreadCount: number | null;
+  lastMessage: {
+    user: UserType
+    time: string
+    content: string
+  }
+};
