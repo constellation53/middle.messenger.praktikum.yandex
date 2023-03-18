53c769ac5d824db2c9c46d3f870f5436a0486368
constellation53 <almahanov567@gmail.com>
2023-03-18 13:48:30 +0600

    chore(chat): increase count of chats

--- src/modules/chat/components/chatList/index.ts
+++ src/modules/chat/components/chatList/index.ts
@@ -171,6 +171,162 @@ const chats: ChatItemType[] = [
         + 'Так увлёкся работой по курсу, что совсем забыл его анонсир ',
     },
   },
+  {
+    id: 123,
+    title: 'Андрей',
+    avatar: null,
+    unreadCount: 15,
+    lastMessage: {
+      user: {
+        firstName: 'Андрей',
+        secondName: 'Pupkin',
+        avatar: null,
+        email: 'my@email.com',
+        login: 'userLogin',
+        phone: '8(911)-222-33-22',
+      },
+      time: '10:49',
+      content: 'Изображение',
+    },
+  },
+  {
+    id: 1234,
+    title: 'Киноклуб',
+    avatar: null,
+    unreadCount: null,
+    lastMessage: {
+      user: {
+        firstName: 'Nurzhan',
+        secondName: 'Pupkin',
+        avatar: null,
+        email: 'my@email.com',
+        login: 'userLogin',
+        phone: '8(911)-222-33-22',
+      },
+      time: '12:00',
+      content: 'Cтикер',
+    },
+  },
+  {
+    id: 123,
+    title: 'Илья',
+    avatar: null,
+    unreadCount: 4,
+    lastMessage: {
+      user: {
+        firstName: 'Илья',
+        secondName: 'Pupkin',
+        avatar: null,
+        email: 'my@email.com',
+        login: 'userLogin',
+        phone: '8(911)-222-33-22',
+      },
+      time: '15:12',
+      content: 'Друзья, у меня для вас особенный выпуск новостей!'
+        + 'Друзья, у меня для вас особенный выпуск новостей!'
+        + 'Друзья, у меня для вас особенный выпуск новостей!',
+    },
+  },
+  {
+    id: 1234,
+    title: 'Вадим',
+    avatar: null,
+    unreadCount: null,
+    lastMessage: {
+      user: {
+        firstName: 'Nurzhan',
+        secondName: 'Pupkin',
+        avatar: null,
+        email: 'my@email.com',
+        login: 'userLogin',
+        phone: '8(911)-222-33-22',
+      },
+      time: 'Пт',
+      content: 'Круто!',
+    },
+  },
+  {
+    id: 123,
+    title: 'тет-а-теты',
+    avatar: null,
+    unreadCount: null,
+    lastMessage: {
+      user: {
+        firstName: 'Илья',
+        secondName: 'Pupkin',
+        avatar: null,
+        email: 'my@email.com',
+        login: 'userLogin',
+        phone: '8(911)-222-33-22',
+      },
+      time: 'Ср',
+      content: 'И Human Interface Guidelines и Material Design '
+        + 'рекомендуют И Human Interface Guidelines и Material Design '
+        + 'рекомендуют И Human Interface Guidelines и Material Design '
+        + 'рекомендуют',
+    },
+  },
+  {
+    id: 123,
+    title: '1, 2, 3',
+    avatar: null,
+    unreadCount: null,
+    lastMessage: {
+      user: {
+        firstName: 'Илья',
+        secondName: 'Pupkin',
+        avatar: null,
+        email: 'my@email.com',
+        login: 'userLogin',
+        phone: '8(911)-222-33-22',
+      },
+      time: 'Пн',
+      content: 'Миллионы россиян ежедневно проводят десятки '
+        + 'часов свое Миллионы россиян ежедневно проводят'
+        + 'десятки часов свое Миллионы россиян ежедневно '
+        + 'проводят десятки часов свое',
+    },
+  },
+  {
+    id: 123,
+    title: 'Design Destroyer',
+    avatar: null,
+    unreadCount: null,
+    lastMessage: {
+      user: {
+        firstName: 'Илья',
+        secondName: 'Pupkin',
+        avatar: null,
+        email: 'my@email.com',
+        login: 'userLogin',
+        phone: '8(911)-222-33-22',
+      },
+      time: 'Пн',
+      content: 'В 2008 году художник Jon Rafman  начал собирать '
+        + 'В 2008 году художник Jon Rafman  начал собирать '
+        + 'В 2008 году художник Jon Rafman  начал собирать',
+    },
+  },
+  {
+    id: 123,
+    title: 'Day.',
+    avatar: null,
+    unreadCount: 999,
+    lastMessage: {
+      user: {
+        firstName: 'Илья',
+        secondName: 'Pupkin',
+        avatar: null,
+        email: 'my@email.com',
+        login: 'userLogin',
+        phone: '8(911)-222-33-22',
+      },
+      time: '1 Мая 2020',
+      content: 'Так увлёкся работой по курсу, что совсем забыл его анонсир '
+        + 'Так увлёкся работой по курсу, что совсем забыл его анонси '
+        + 'Так увлёкся работой по курсу, что совсем забыл его анонсир ',
+    },
+  },
 ];
 
 export class ChatListComponent extends Block {
