27b28d134d76a57fdf362138edea1558913b001e
constellation53 <almahanov567@gmail.com>
2023-03-19 21:48:40 +0600

    fix(chat): fix time position

--- src/modules/chat/components/dialog/message/index.ts
+++ src/modules/chat/components/dialog/message/index.ts
@@ -7,6 +7,7 @@ import template from './index.hbs';
 // Other
 import { MessageType } from '../types';
 import * as styles from '../styles/index.module.scss';
+import { calculateLines } from '../utils/calculateLines';
 
 type PropsType = MessageType;
 
@@ -15,7 +16,17 @@ export class MessageComponent extends Block<PropsType> {
     super(props);
   }
 
+  componentDidMount(): void {
+    if (!this.props.isImage) {
+      const lines = calculateLines(`[data-id="${this.id}"] .${styles.content}`);
+
+      this.getContent()?.classList.toggle(styles.oneLine, lines <= 1);
+    }
+  }
+
   render(): HTMLElement {
-    return this.compile(template, { ...this.props, styles });
+    return this.compile(template, {
+      ...this.props, styles, settings: { withInternalID: true },
+    });
   }
 }
