0b36c3844283df1efda5b0f81a8173b47d05dc34
constellation53 <almahanov567@gmail.com>
2023-03-21 12:10:11 +0600

    feat(auth): rule for login

--- src/modules/auth/registrationForm/utils/loginValidationRule/index.ts
+++ src/modules/auth/registrationForm/utils/loginValidationRule/index.ts
@@ -10,12 +10,29 @@ export class LoginValidationRule extends ValidationRule {
   }
 
   execute(value: string): ErrorType {
-    const lettersRegex = /^[A-Za-z][A-Za-z0-9]*$/;
+    const symbols = /^[A-Za-z][A-Za-z0-9]*$/;
+    const minChar = /^[0-9a-zA-Z]{4,}$/;
 
-    if (!lettersRegex.test(value)) {
-      return { error: true, message: 'Email должен состоять только из английских букв и цифр' };
-    }
+    const rules: [RegExp, string][] = [
+      [
+        symbols,
+        'Логин должен состоять только из английских букв и цифр',
+      ],
+      [
+        minChar,
+        'Логин должен состоять как минимум из английских 4',
+      ],
+    ];
 
-    return { error: false };
+    const result: ErrorType = { error: false };
+
+    rules.forEach(([regex, message]) => {
+      if (!regex.test(value)) {
+        result.error = true;
+        result.message = message;
+      }
+    });
+
+    return result;
   }
 }
