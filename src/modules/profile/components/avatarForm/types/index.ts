b9d17c3949716bbf84c9363dc02198c855016761
constellation53 <almahanov567@gmail.com>
2023-03-23 23:27:52 +0600

    fix(profile_: typing of avatar form

--- src/modules/profile/components/avatarForm/types/index.ts
+++ src/modules/profile/components/avatarForm/types/index.ts
@@ -1,3 +1,7 @@
 export type FieldsType = 'avatar';
 
-export type FormFieldsType = Record<FieldsType, string>;
+export type FormFieldsType = Record<FieldsType, File>;
+
+export type ValidatorType = {
+  avatar: [File];
+};
--- src/modules/profile/components/avatarForm/utils/avatarValidator/index.ts
+++ src/modules/profile/components/avatarForm/utils/avatarValidator/index.ts
@@ -3,12 +3,12 @@ import { Validator } from '../../../../../core/utils/validator';
 
 // Other
 import { ValidatorRuleType } from '../../../../../core/utils/validationRule/types';
-import { FieldsType } from '../../types';
+import { ValidatorType } from '../../types';
 import { Avatar } from '../validationRules/avatar';
 
-export class AvatarValidator extends Validator<FieldsType> {
+export class AvatarValidator extends Validator<ValidatorType> {
   constructor() {
-    const validators = new Map<FieldsType, ValidatorRuleType>([
+    const validators = new Map<keyof ValidatorType, ValidatorRuleType>([
       [
         'avatar',
         new Avatar(),
