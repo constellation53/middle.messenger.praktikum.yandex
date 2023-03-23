9b94b81d2ce657960514616076f803ca5be792c2
constellation53 <almahanov567@gmail.com>
2023-03-24 01:16:54 +0600

    fix(core): password validation

--- src/modules/core/utils/validationRules/password.ts
+++ src/modules/core/utils/validationRules/password.ts
@@ -5,18 +5,34 @@ import ValidationRule from '../validationRule';
 import { ErrorType } from '../validator/types';
 import { getError } from '../validationRule/helpers/getError';
 
-export class Password extends ValidationRule {
+export class Password extends ValidationRule<[string]> {
   constructor() {
     super();
   }
 
   execute(password: string): ErrorType {
-    const symbols = /^(\S{6,})$/;
+
+    const minChar = /^.{8,}$/;
+    const maxChar = /^.{8,40}$/;
+    const atLeastOneCapitalLetter = /(?=.*[A-Z])/;
+    const atLeastOneNumber = /(?=.*\d)/;
 
     const rules: [RegExp, string][] = [
       [
-        symbols,
-        'Пароль должен состоять как минимум из 6 символов (пробелы запрещены)',
+        minChar,
+        'Минимальное количество символов - 8',
+      ],
+      [
+        maxChar,
+        'Максимальное количество символов - 40',
+      ],
+      [
+        atLeastOneCapitalLetter,
+        'Требуется хотя бы одна заглавная буква',
+      ],
+      [
+        atLeastOneNumber,
+        'Требуется хотя бы одно число',
       ],
     ];
 
